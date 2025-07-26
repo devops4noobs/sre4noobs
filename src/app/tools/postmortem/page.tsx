2aa8a00d00ee86a2cb35bec39b3a101d56cb2aaf
devops4noobs <devops4noobs@gmail.com>
2025-07-26 13:04:52 +0300

    try fix 1

--- src/app/tools/postmortem/page.tsx
+++ src/app/tools/postmortem/page.tsx
@@ -1,6 +1,3 @@
-import PostmortemForm from '@/components/PostmortemForm';
-import ErrorBoundary from '@/components/ErrorBoundary';
-
 export default function PostmortemPage() {
   return (
     <main className="container mx-auto p-8">
